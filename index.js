9699a384803a8bc0df7fe735953906dc103a3781
Harsh raj <140217747+Rajharsh36@users.noreply.github.com>
2025-03-19 21:42:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+// Serve static files (CSS, images, etc.) from the current directory
+app.use(express.static(__dirname));
+
+// Serve the portfolio HTML file
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+app.get("/img", (req, res) => {
+    res.sendFile(path.join(__dirname, "img.jpg"));
+});
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
